249af22cd93293335d3114d5bd72674622301cfe
Hena Jose <henajose@users.noreply.github.com>
2014-07-09 12:46:43 +0530

    Update Plot1_R.R

--- Plot1_R.R
+++ Plot1_R.R
@@ -1,5 +1,9 @@
 
-  
+  Powernew <- read.csv("Power.csv", header = TRUE, sep = ",", dec=".")
+  Powernew$Date <-as.Date(Powernew$Date, format = "%d-%m-%Y")
+   Powernew$DateTime <- paste (Powernew$Date, Powernew$Time)
+   Powernew$day<- weekdays(Powernew$Date)
+   Powernew$DateTime<-as.POSIXlt(Powernew$DateTime)
   
   hist(Powernew$Global_active_power,
        main="Global Active Power",
